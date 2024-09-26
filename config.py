a00f1a585a7c35a1d2b0e7848762fcdec8e8630a
Howtog41 <howtog41@gmail.com>
2024-09-26 15:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = 'YOUR_BOT_TOKEN'
-ADMIN_ID = 123456789
+TOKEN = '5645711998:AAE8oAHzKi07iqcydKPnuFjzknlVa2MxxUQ'
+ADMIN_ID = 5018200809
 MONGO_URI = 'mongodb+srv://terabox255:0GptJ2lD2NdW1XtN@cluster0.1gfjb8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
