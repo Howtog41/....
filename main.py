1ae47959e429a1f9de03dd8b683a370ba7ed16d0
Howtog41 <howtog41@gmail.com>
2024-09-28 21:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from handlers.csv_handler import upload_csv_command, handle_csv_file
 from handlers.poll_handler import choose_destination, channel_callback, send_all_polls
 from handlers.channel_handler import set_channel, channels, channel_management_callback
 from handlers.authorization_handler import authorize
+from handlers.channel_change_handler import change_channel, set_channel, receive_message, done  # Import the new handler functions
 from config import TOKEN
 
 # Define states for the conversation
 UPLOAD_CSV, CHOOSE_DESTINATION, CHOOSE_CHANNEL = range(3)
+CHANNEL, MESSAGE = range(2)  # States for the change_channel conversation
 
 # Set up logging
 logging.basicConfig(
@@ -19,8 +21,8 @@ logging.basicConfig(
 def main():
     application = Application.builder().token(TOKEN).build()
 
-    # Define the conversation handler with the states
-    conversation_handler = ConversationHandler(
+    # Define the CSV conversation handler
+    csv_conversation_handler = ConversationHandler(
         entry_points=[CommandHandler("uploadcsv", upload_csv_command)],  # Entry point for the CSV upload command
         states={
             UPLOAD_CSV: [MessageHandler(filters.Document.FileExtension("csv"), handle_csv_file)],  # CSV file handler
@@ -31,8 +33,20 @@ def main():
         allow_reentry=True
     )
 
+    # Define the channel change conversation handler
+    channel_change_conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("change_channel", change_channel)],  # Start with /change_channel
+        states={
+            CHANNEL: [MessageHandler(filters.TEXT & ~filters.COMMAND, set_channel)],  # Set the new channel name
+            MESSAGE: [MessageHandler(filters.ALL & ~filters.COMMAND, receive_message)]  # Receive messages
+        },
+        fallbacks=[CommandHandler("done", done)],  # End the conversation with /done
+        allow_reentry=True
+    )
+
     # Add handlers to the application
-    application.add_handler(conversation_handler)
+    application.add_handler(csv_conversation_handler)  # CSV upload handler
+    application.add_handler(channel_change_conversation_handler)  # Change channel handler
     application.add_handler(CommandHandler("start", start))  # /start command
     application.add_handler(CommandHandler("setchannel", set_channel))  # /setchannel command
     application.add_handler(CommandHandler("channels", channels))  # /channels command
