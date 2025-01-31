8be0f87d39c2bac19616d37a9c9bc8011a679df8
Howtog41 <howtog41@gmail.com>
2025-02-01 04:36:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '7961958205:AAExxM3Sbb_fvUDSMc2JmOtlTFgCOJhi8gA'
-ADMIN_ID = 768821534
+TOKEN = '7938208817:AAFWHdFfc8_NCyJrP_MhYpAZ7L3_9RHwgnA'
+ADMIN_ID = 1922012735
 MONGO_URI = 'mongodb+srv://latestkoreandraama:nydRMd6x7tVplGNV@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
