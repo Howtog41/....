57e07b678f6fb778dcfefc1b12ef3ea6d16ff42d
Howtog41 <howtog41@gmail.com>
2025-02-01 04:41:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = '7938208817:AAFWHdFfc8_NCyJrP_MhYpAZ7L3_9RHwgnA'
 ADMIN_ID = 1922012735
-MONGO_URI = 'mongodb+srv://latestkoreandraama:nydRMd6x7tVplGNV@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+MONGO_URI = 'mongodb+srv://latestkoreandraama:UjebJR51Dki7Ili2@cluster0.nnnuejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
