65a1341f8159c534a64bb7d8acbdcd0964305de3
Howtog41 <howtog41@gmail.com>
2025-02-01 05:21:56 +0530

    Update db.py

--- helpers/db.py
+++ helpers/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from config import MONGO_URI
-
+from helpers.db import users_collection
 client = MongoClient(MONGO_URI)
 db = client['quiz_bot']
 users_collection = db['users']
