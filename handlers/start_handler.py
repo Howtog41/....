32d4d4241a60786616fe9628ea80322c6e6348dd
Howtog41 <howtog41@gmail.com>
2025-08-01 22:05:54 +0530

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,42 +1,38 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from helpers.db import users_collection
+from datetime import datetime, timedelta
 import logging
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.debug("Received /start command")
     user_id = update.effective_user.id
     user_info = users_collection.find_one({'user_id': user_id})
+    now = datetime.now()
 
     if user_info:
-        await update.message.reply_text(
-            "Welcome back! ʜɪ ᴛʜᴇʀᴇ!  \n"
-
-            "➻ɪ'ᴍ ʏᴏᴜʀ ᴍᴄQ ʙᴏᴛ. 🤖 \n"
-
-            "➻ᴜᴘʟᴏᴀᴅ ʏᴏᴜʀ ᴄꜱᴠ 📄ꜰɪʟᴇ ᴡɪᴛʜ ᴛʜᴇ ꜰᴏʟʟᴏᴡɪɴɢ ᴄᴏʟᴜᴍɴꜱ: \n"
-
-            "👉Qᴜᴇꜱᴛɪᴏɴ, ᴏᴘᴛɪᴏɴ ᴀ, ᴏᴘᴛɪᴏɴ ʙ, ᴏᴘᴛɪᴏɴ ᴄ, ᴏᴘᴛɪᴏɴ ᴅ, ᴀɴꜱᴡᴇʀ, ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ.\n"
-
-            "Use Command: -🔰 /uploadcsv."
-
-            "➻ ɪ'ʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ɪɴᴛᴏ ᴍᴜʟᴛɪᴘʟᴇ-ᴄʜᴏɪᴄᴇ Qᴜᴇꜱᴛɪᴏɴꜱ ꜰᴏʀ ʏᴏᴜ! \n"
-
-            "• Mᴀɪɴᴛᴀɪɴᴇʀ: @How_to_Google \n"
-        )
+        expires_on = user_info.get('expires_on')
+        if expires_on and expires_on < now:
+            await update.message.reply_text("⚠️ Your free trial has expired.\nContact admin for full access.")
+        else:
+            await update.message.reply_text(
+                "Welcome back! ʜɪ ᴛʜᴇʀᴇ!  \n"
+                "➻ɪ'ᴍ ʏᴏᴜʀ ᴍᴄQ ʙᴏᴛ. 🤖 \n"
+                "➻ᴜᴘʟᴏᴀᴅ ʏᴏᴜʀ ᴄꜱᴠ 📄ꜰɪʟᴇ...\n"
+                "Use Command: -🔰 /uploadcsv.\n"
+                "• Mᴀɪɴᴛᴀɪɴᴇʀ: @How_to_Google \n"
+            )
     else:
-        users_collection.insert_one({'user_id': user_id})
+        users_collection.insert_one({
+            'user_id': user_id,
+            'authorized': True,
+            'authorized_on': now,
+            'expires_on': now + timedelta(days=3)
+        })
         await update.message.reply_text(
-            "Welcome ! ʜɪ ᴛʜᴇʀᴇ!  \n"
-
-            "➻ɪ'ᴍ ʏᴏᴜʀ ᴍᴄQ ʙᴏᴛ. 🤖 \n"
-
-            "➻ᴜᴘʟᴏᴀᴅ ʏᴏᴜʀ ᴄꜱᴠ 📄ꜰɪʟᴇ ᴡɪᴛʜ ᴛʜᴇ ꜰᴏʟʟᴏᴡɪɴɢ ᴄᴏʟᴜᴍɴꜱ: \n"
-
-            "👉Qᴜᴇꜱᴛɪᴏɴ, ᴏᴘᴛɪᴏɴ ᴀ, ᴏᴘᴛɪᴏɴ ʙ, ᴏᴘᴛɪᴏɴ ᴄ, ᴏᴘᴛɪᴏɴ ᴅ, ᴀɴꜱᴡᴇʀ, ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ.\n"
-
-            "Use Command: -🔰 /uploadcsv."
-
-            "➻ ɪ'ʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ɪɴᴛᴏ ᴍᴜʟᴛɪᴘʟᴇ-ᴄʜᴏɪᴄᴇ Qᴜᴇꜱᴛɪᴏɴꜱ ꜰᴏʀ ʏᴏᴜ! \n"
-
+            "🎉 ʜɪ ᴛʜᴇʀᴇ!  \n"
+            "➻ You’ve been given *3 days free trial access.*\n"
+            "➻ᴜᴘʟᴏᴀᴅ ʏᴏᴜʀ ᴄꜱᴠ 📄ꜰɪʟᴇ...\n"
+            "Use Command: -🔰 /uploadcsv.\n"
             "• Mᴀɪɴᴛᴀɪɴᴇʀ: @How_to_Google \n"
         )
